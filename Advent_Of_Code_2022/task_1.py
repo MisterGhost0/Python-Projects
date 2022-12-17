e557ff537ca920d99b037095008cb10a500fddaf
batuhansazak <ic19b029@technikum-wien.at>
2022-12-17 22:29:46 +0100

    updated task_1.py, added .txt file

--- Advent_Of_Code_2022/task_1.py
+++ Advent_Of_Code_2022/task_1.py
@@ -1,3 +1,23 @@
-# Task 1
+# Task 1 - Calorie Counting
+# Solution by Batuhan Sazak
 
-print("Hello World!")
+import os
+
+print("Start!")
+
+def findTheElfWithMostCalories():
+    print("Hello from function!")
+    with open("calorie_list.txt","r") as fileHandler:
+        file_content = fileHandler.read()
+        #print("File Content:\n", file_content)
+
+        list_content = file_content.split("\n")
+    
+    fileHandler.close()
+
+    print("List Content:\n", list_content)
+
+    list_content.count('\n\n')
+    
+
+findTheElfWithMostCalories()
